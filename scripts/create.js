c01f78343c35de9d25ceb58cc0e66e73b9af4af0
William Manco <wmanco88@gmail.com>
2023-12-18 22:46:59 +0100

    add create and version scripts

---
+++ scripts/create.js
@@ -0,0 +1,96 @@
+import { Tool } from '@toolx/core';
+import { toCamelCase } from '@toolx/core/utils.js';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const args = process.argv.slice(2);
+
+const templateJS = (name) => /*js*/`
+import Tool from '@toolx/core/Tool.server.js';
+
+class Tool${name} extends Tool {
+    async onEveryFile(next, { file, options }) {
+        next(file);
+    }
+}
+
+export default Tool${name}
+`;
+
+
+const templatePackage = (name, version) => /* json */`{
+    "name": "@toolx/tool-${toCamelCase(name).toLowerCase()}",
+    "version": "${version.split('.')[0]}.0.0",
+    "description": "",
+    "main": "Tool${toCamelCase(name, true)}.js",
+    "source": "Tool${toCamelCase(name, true)}.js",
+    "publishConfig": {
+        "access": "public",
+        "main": "Tool${toCamelCase(name, true)}.js"
+    },
+    "type": "module",
+    "sideEffects": false,
+    "dependencies": {
+        "@toolx/core": "^${version}"
+    },
+    "scripts": {
+        "test": "echo 'Error: no test specified' && exit 1"
+    },
+    "repository": {
+        "type": "git",
+        "url": "git+https://github.com/williammanco/toolx.git"
+    },
+    "author": "William Manco",
+    "license": "MIT",
+    "bugs": {
+        "url": "https://github.com/williammanco/toolx/issues"
+    },
+    "homepage": "https://github.com/williammanco/toolx#readme"
+}
+`;
+
+const templateTest = (name) => /*js*/`import { describe, it, expect, beforeEach } from 'vitest';
+import Tool from '../Tool${name}';
+
+describe('Tool${name}', () => {
+    let toolInstance;
+
+    beforeEach(async () => {
+        toolInstance = new Tool();
+    });
+
+    it('should create an instance of Tool', () => {
+        expect(toolInstance).toBeInstanceOf(Tool);
+    });
+});
+`;
+
+const templateReadme = (name) => /*js*/`#tool-${name}`;
+
+const run = async () => {
+    const [name] = args;
+    // const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const dir = `${process.cwd()}/packages/tool-${toCamelCase(name).toLowerCase()}/`;
+    const folder = await Tool.createDir(dir);
+    const nameCamelCase = toCamelCase(name, true);
+
+    const packageInfo = await fs.promises.readFile(`${process.cwd()}/packages/core/package.json`);
+    const packageRootInfo = await fs.promises.readFile(`${process.cwd()}/package.json`);
+    const version = JSON.parse(packageInfo).version;
+
+    if (folder) {
+        await fs.promises.writeFile(`${dir}Tool${nameCamelCase}.js`, templateJS(nameCamelCase));
+        await fs.promises.writeFile(`${dir}package.json`, templatePackage(name, version));
+
+        await Tool.createDir(path.join(dir, 'test'));
+        await fs.promises.writeFile(`${path.join(dir, 'test')}/Tool${nameCamelCase}.test.js`, templateTest(nameCamelCase));
+        await fs.promises.writeFile(`${dir}/README.md`, templateReadme(name));
+
+        const packageRootInfoEdited = JSON.parse(packageRootInfo);
+        packageRootInfoEdited.workspaces.push(`packages/tool-${name}`)
+        await fs.promises.writeFile(`${process.cwd()}/package.json`, JSON.stringify(packageRootInfoEdited, null, 4));
+    }
+};
+
+run();
+
---
+++ scripts/version.js
@@ -0,0 +1,79 @@
+import { exec } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+async function init() {
+    try {
+        const args = process.argv.slice(2);
+        const packageName = args[0];
+        const type = args[1] || 'patch';
+
+        if (!packageName) {
+            throw new Error('Please provide a package name.');
+        }
+
+        if (packageName === 'all' || type === "major") {
+            const packageNames = await getAllPackageNames();
+            for (const pkgName of packageNames) {
+                await bumpVersion(pkgName, type);
+            }
+        } else {
+            await bumpVersion(packageName, type);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function bumpVersion(packageName, type) {
+    const packageDir = path.join(process.cwd(), 'packages', packageName);
+    const packageJsonPath = path.join(packageDir, 'package.json');
+
+    await execCommand(`npm version ${type}`);
+
+    const prevPackageJson = JSON.parse(await fs.promises.readFile(packageJsonPath, 'utf-8'));
+
+    await execCommand(`cd ${packageDir} && npm version ${type}`);
+
+    const updatedPackageJson = JSON.parse(await fs.promises.readFile(packageJsonPath, 'utf-8'));
+    const packageVersion = updatedPackageJson.version;
+
+    console.log(`Version bumped for ${packageName} from v${prevPackageJson.version} to v${packageVersion}`);
+
+    await execCommand(`git tag ${packageName}/${packageVersion}`);
+    // await execCommand(`git push origin ${packageName}/${packageVersion}`);
+
+    console.log(`Tag created and pushed for ${packageName} v${packageVersion}`);
+}
+
+async function execCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(new Error(`Command failed: ${command}\nError: ${error.message}`));
+            } else {
+                resolve(stdout);
+            }
+        });
+    });
+}
+
+async function getAllPackageNames() {
+    const packagesDir = path.join(process.cwd(), 'packages');
+    const packageNames = [];
+
+    const entries = await fs.promises.readdir(packagesDir);
+
+    for (const entry of entries) {
+        const packageDirPath = path.join(packagesDir, entry);
+        const stats = await fs.promises.stat(packageDirPath);
+
+        if (stats.isDirectory()) {
+            packageNames.push(entry);
+        }
+    }
+
+    return packageNames;
+}
+
+init();
