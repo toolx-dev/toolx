8e3322bd045067b79ba2ed77f8e23c0694beb9ba
William Manco <wmanco88@gmail.com>
2023-12-25 21:27:57 +0100

    fix test issues with singleThread options

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        include: process.env.RUNNER_TEMP ? ['**/*.test.js', '!**/combine/test/**/*.test.js'] : ['**/*.test.js']
+        include: ['**/*.test.js'],
+        poolOptions: {
+            threads: {
+                singleThread: true,
+            },
+        },
     },
 });
